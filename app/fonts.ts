0d977cd1354bcac0c32e6b6ade71142d72a5c96b
netmehul <mehul.m@netizens.email>
2026-09-01 17:29:33 +0530

    Updated the font ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,42 +3,42 @@ import localFont from "next/font/local";
 export const avant = localFont({
   src: [
     {
-      path: "../fonts/AvantT-TRIAL-Thin.woff2",
+      path: "../fonts/Avantt-TRIAL-Thin.woff2",
       weight: "100",
       style: "normal",
     },
     {
-      path: "../fonts/AvantT-TRIAL-Light.woff2",
+      path: "../fonts/Avantt-TRIAL-Light.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../fonts/AvantT-TRIAL-Regular.woff2",
+      path: "../fonts/Avantt-TRIAL-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../fonts/AvantT-TRIAL-Medium.woff2",
+      path: "../fonts/Avantt-TRIAL-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../fonts/AvantT-TRIAL-SemiBold.woff2",
+      path: "../fonts/Avantt-TRIAL-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../fonts/AvantT-TRIAL-Bold.woff2",
+      path: "../fonts/Avantt-TRIAL-Bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../fonts/AvantT-TRIAL-ExtraBold.woff2",
+      path: "../fonts/Avantt-TRIAL-ExtraBold.woff2",
       weight: "800",
       style: "normal",
     },
     {
-      path: "../fonts/AvantT-TRIAL-Heavy.woff2",
+      path: "../fonts/Avantt-TRIAL-Heavy.woff2",
       weight: "900",
       style: "normal",
     },
