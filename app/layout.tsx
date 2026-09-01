c7f5ca8bff18e468d27a41bb64a0ee1d8f28336c
netmehul <mehul.m@netizens.email>
2026-09-01 17:47:41 +0530

    meta data updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,26 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Vision",
   description: "The Vision will be next project",
+  openGraph: {
+    title: "Vision",
+    description: "The Vision will be next project",
+    url: "https://vision-m.vercel.app/",
+    siteName: "Vision",
+    images: [
+      {
+        url: "https://vision-m.vercel.app/og.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Vision",
+    description: "The Vision will be next project",
+  },
 };
 
 export default function RootLayout({
